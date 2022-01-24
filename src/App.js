1013d1d3b5450d2a068b671c2e3fa8c27a4d0cf5
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-24 21:20:44 +0100

    [Header] Tinder header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import  Header  from "./Header";
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <h3>Lets build the MERN TINDER clone</h3>
+
+      {/* Header */}
+      <Header />
+      {/* TinderCards */}
+
+      {/* SwipeButtons */}
     </div>
   );
 }
