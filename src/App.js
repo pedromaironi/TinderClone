7173e815e32654c82a28349408b15271470e36f8
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-24 22:20:58 +0100

    [Tinder-Cards] Adding the styles

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <h3>Lets build the MERN TINDER clone</h3>
-
       {/* Header */}
       <Header />
       {/* TinderCards */}
